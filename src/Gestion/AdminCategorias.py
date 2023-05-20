ae0ff72bdb8dd43bc54679d052127e09dc7b3473
Samuel Espitia <samuelespitia88@gmail.com>
2023-05-20 18:53:57 -0500

    Arregla fallos

--- src/Gestion/AdminCategorias.py
+++ src/Gestion/AdminCategorias.py
@@ -1,186 +1,174 @@
-from src.Modelo.Categoria import Categoria
-from src.Conex.Conexion import Conexion
+import sys
+from PyQt6.QtWidgets import QMainWindow, QApplication, QMessageBox, QTableWidgetItem
 
-class AdminCategorias:
+from src.Conex.Conexion import Conexion
+from src.GUIS.DCategorias import Ui_MainWindow
+
+
+class AdminCategorias(QMainWindow):
 
     def __init__(self):
+        super(AdminCategorias, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
         self.con = Conexion()
         self.miConexion = self.con.conectar()
+        print('Objeto tipo AdminCategoria creado y listo para usarse..!!')
 
-        print('Objeto tipo AdminCategorias creado')
-        self.menu()
+        self.ui.PBTodas.clicked.connect(self.verTodas)
+        self.ui.PBBuscar.clicked.connect(self.buscarCategoria)
+        self.ui.PBAgregar.clicked.connect(self.agregaCategoria)
+        self.ui.PBModificar.clicked.connect(self.modifyCategoria)
+        self.ui.PBEliminar.clicked.connect(self.eliminaCategoria)
+        self.ui.PBSalir.clicked.connect(self.cerrarConexion)
 
+        self.verTodas()
 
-    def menu(self):
-        opcion = -1
-        while opcion != 0:
-
-            print('\n===============')
-            print(' Categorias')
-            print('===============')
-            print("0. Salir")
-            print("1. Nueva Categoría")
-            print("2. Ver todas las categorías")
-            print("3. Buscar categoría")
-            print("4. Eliminar categoría")
-            print("5. Modificar categoría")
-
-            opcion = int(input("Opción: "))
-            print()
-
-            if opcion == 0:
-                self.miConexion = self.con.desconectar()
-                print("Fin del menu de Categorías")
-
-            elif opcion == 1:
-                self.nuevaCategoria()
-            elif opcion == 2:
-                self.verTodas()
-            elif opcion == 3:
-                self.buscarCategoria()
-            elif opcion == 4:
-                self.eliminarCategoria()
-            elif opcion == 5:
-                self.modificarCategoria()
-            else:
-                print('Esa opción no existe!')
-
-
-    def nuevaCategoria(self):
-
-        id = int(input("¿Cuál es el ID?: "))
-
-        if self.existeId(id):
-            print("Ya existe una categoría con ese código.")
-            return
-
-        nombre = input("¿Cuál es el nombre?: ")
-
-        try:
-            mycursor = self.miConexion.cursor()
-
-            mycursor.callproc('newCategoria', [id, nombre])
-            self.miConexion.commit()
-
-            print('La categoría ha sido creada!')
-            mycursor.close()
-
-        except Exception as miError:
-            print('Fallo ejecutando el procedimiento')
-            print(miError)
-
+    def cerrarConexion(self):
+        self.con.desconectar()
+        self.close()
 
     def verTodas(self):
-
-        print("---Lista de Categorías---\n")
-
         cant = 0
         try:
             mycursor = self.miConexion.cursor()
             mycursor.callproc('allCategorias')
 
+            total = self.ui.TWTabla.rowCount()
+            for rep in range(total):
+                self.ui.TWTabla.removeRow(0)
+
             for result in mycursor.stored_results():
-
                 for (idCategoria, nombre) in result:
-                    laCategoria = Categoria(idCategoria, nombre)
-                    laCategoria.toString()
-                    cant = cant + 1
+                    self.ui.TWTabla.insertRow(cant)
 
+                    celdaId = QTableWidgetItem(str(idCategoria))
+                    celdaNombre = QTableWidgetItem(nombre)
+
+                    self.ui.TWTabla.setItem(cant, 0, celdaId)
+                    self.ui.TWTabla.setItem(cant, 1, celdaNombre)
+
+                    cant = cant + 1
             if cant == 0:
-                print("No hay categorías registradas")
+                QMessageBox.information(self, "Ver Todas", "No hay categorias registradas..!!")
 
             mycursor.close()
-
         except Exception as miError:
-            print('Fallo ejecutando el procedimiento!')
+            QMessageBox.warning(self, "Error", 'Fallo ejecutando el procedimiento')
             print(miError)
 
-
     def buscarCategoria(self):
+        cant = 0
 
-        id = int(input('Digite el ID a buscar: '))
-        if not self.existeId(id):
-            print("Esa categoría no existe")
+        idCategoriaSearch = self.ui.SBId.value()
+        if not self.existeIdCategoria(idCategoriaSearch):
+            QMessageBox.information(self, "Buscar", "La categoria no existe")
         else:
             try:
                 mycursor = self.miConexion.cursor()
-                mycursor.callproc('getCategoria', [id])
+                mycursor.callproc('getCategoria', [idCategoriaSearch])
+
+                total = self.ui.TWTabla.rowCount()
+                for rep in range(total):
+                    self.ui.TWTabla.removeRow(0)
 
                 for result in mycursor.stored_results():
                     for (idCategoria, nombre) in result:
-                        laCategoria = Categoria(idCategoria, nombre)
-                        laCategoria.toString()
+                        self.ui.TWTabla.insertRow(cant)
 
-            except Exception as miError:
-                print('Fallo ejecutando el procedimiento')
-                print(miError)
+                        celdaId = QTableWidgetItem(str(idCategoria))
+                        celdaNombre = QTableWidgetItem(nombre)
 
+                        self.ui.TWTabla.setItem(cant, 0, celdaId)
+                        self.ui.TWTabla.setItem(cant, 1, celdaNombre)
 
-    def eliminarCategoria(self):
-
-        id = int(input('Digite el ID a eliminar: '))
-        if not self.existeId(id):
-            print("Esa categoría no existe, no se puede eliminar")
-
-        else:
-            try:
-                mycursor = self.miConexion.cursor()
-                mycursor.callproc('delCategoria', [id])
-                self.miConexion.commit()
-
-                print('La categoría ha sido eliminada..!!')
-
-            except Exception as miError:
-                print('Fallo ejecutando el procedimiento')
-                print(miError)
-
-
-    def modificarCategoria(self):
-
-        id = int(input("¿Digite el ID a modificar:  "))
-        print()
-
-        if not self.existeId(id):
-            print("Esa categoría no existe")
-        else:
-            try:
-                mycursor = self.miConexion.cursor()
-                mycursor.callproc('getCategoria', [id])
-
-                for result in mycursor.stored_results():
-                    for (idCategoria, nombre) in result:
-                        laCategoria = Categoria(idCategoria, nombre)
-
-                newId = int(input("¿Cuál es el nuevo Id?: "))
-
-                if self.existeId(newId) and newId != laCategoria.getIdCategoria():
-                    print("Ese Id ya existe")
-                    mycursor.close()
-                    return
-
-                newNombre = input("¿Cuál es el nuevo nombre?: ")
-
-                mycursor.callproc('modCategoria', [newId, newNombre, id])
-                self.miConexion.commit()
-                print("\nHas modificado la categoría con éxito.")
                 mycursor.close()
 
             except Exception as miError:
-                print('Fallo ejecutando el procedimiento')
+                QMessageBox.warning(self, "Error", 'Fallo ejecutando el procedimiento')
                 print(miError)
 
-    def existeId(self, id):
+    def agregaCategoria(self):
+        idCategoriaNew = self.ui.SBId2.value()
+        if self.existeIdCategoria(idCategoriaNew):
+            QMessageBox.information(self, "Agregar Categoria", "La categoria ya existe, no se puede repetir")
+        else:
+            nombreNew = self.ui.LENombre.text()
+            try:
+                mycursor = self.miConexion.cursor()
+                mycursor.callproc("newCategoria", [idCategoriaNew, nombreNew])
+                self.miConexion.commit()
+                QMessageBox.information(self, "Agregar Categoria", "La categoria ha sido creada")
+                mycursor.close()
+
+            except Exception as miError:
+                QMessageBox.warning(self, "Agregar Categoria", "La categoria ya existe, no se puede repetir")
+                print(miError)
+
+        self.verTodas()
+
+
+    def eliminaCategoria(self):
+
+        idCategoriaDel = self.ui.SBId.value()
+        if not self.existeIdCategoria(idCategoriaDel):
+            QMessageBox.information(self, "Eliminar", "La categoria no existe, no se puede eliminar")
+        else:
+            try:
+                mycursor = self.miConexion.cursor()
+                mycursor.callproc('delCategoria', [idCategoriaDel])
+                self.miConexion.commit()
+
+                QMessageBox.information(self, "Eliminar", 'La categoria ha sido eliminada..!!')
+                mycursor.close()
+            except Exception as miError:
+                QMessageBox.warning(self, "Eliminar", 'Fallo ejecutando el procedimiento')
+                print(miError)
+
+        self.verTodas()
+
+    def modifyCategoria(self):
+        idCategoriaOld = self.ui.SBId2.value()
+        if not self.existeIdCategoria(idCategoriaOld):
+            QMessageBox.information(self, "Modificar", "La categoria no existe")
+        else:
+            idCategoriaNew = self.ui.SBNuevoId.value()
+            if idCategoriaNew != idCategoriaOld and self.existeIdCategoria(idCategoriaNew):
+                QMessageBox.information(self, "Modificar",
+                                        "Ya existe una categoria con ese ID, No se puede modificar")
+            else:
+                nombreNew = self.ui.LENombre.text()
+                try:
+                    mycursor = self.miConexion.cursor()
+                    mycursor.callproc('modCategoria', [idCategoriaNew, nombreNew, idCategoriaOld])
+                    self.miConexion.commit()
+                    QMessageBox.information(self, "Modificar", 'La categoria ha sido modificada..!!')
+                    mycursor.close()
+
+                    self.verTodas()
+
+                except Exception as miError:
+                    QMessageBox.warning(self, "Modificar", 'Fallo ejecutando el procedimiento')
+                    print(miError)
+
+    def existeIdCategoria(self, idCategoria):
         try:
             mycursor = self.miConexion.cursor()
-            query = "SELECT count(*) FROM categorias WHERE idCategoria = %s;"
-            mycursor.execute(query, [id])
+            query = "SELECT count(*) FROM CATEGORIAS WHERE idCategoria = %s"
+            mycursor.execute(query, [idCategoria])
             resultados = mycursor.fetchall()
-
             for registro in resultados:
                 if registro[0] == 1:
                     return True
                 return False
-
         except Exception as miError:
-            print('Fallo ejecutando el programa!')
+            QMessageBox.information(self, "Existe Categoria", 'Fallo ejecutando el procedimiento')
             print(miError)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    ventanaAdminCategorias = AdminCategorias()
+    ventanaAdminCategorias.show()
+    sys.exit(app.exec())
